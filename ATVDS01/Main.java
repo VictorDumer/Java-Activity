726d9e57ce82cf6e2ab59a34e7ae0100ce3dd532
VictorDumer <victorgabrieldummersouza@gmail.com>
2025-03-15 22:09:08 -0300

    Mudança no código / código em tése funcionando todavia apresentando ainda assim erro de index.

--- ATVDS01/Main.java
+++ ATVDS01/Main.java
@@ -4,53 +4,108 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] Args){
         Scanner Entrada = new Scanner(System.in);
-        int op; 
-        Produto produtos = new Produto ();
-        System.out.println("Bem vindo ao Market Place, o que você deseja hoje ?");
+        int op;
+        int req=0;
+        int info=0;
+        int remove=0;
+
+        String preco, resolve, resposta;
+        String resultado="";
+        String endereco,imovel;
+        boolean avenda= false;
+        boolean nova = false;
+
+        ArrayList<String> produtos = new ArrayList<>();
+        
+        do{
+        System.out.println("\n" + //
+                        "Bem vindo ao Hub central do Market Place, o que você deseja hoje ?");
         System.out.println(" ");
         System.out.println("1 - Criar um produto");
         System.out.println("2 - Excluir um produto");
         System.out.println("3 - Listar todos os produtos");
         System.out.println("4 - Sair");
         op = Entrada.nextInt();
-        
-        switch (op) {
-            case 1:
-                System.out.println("Digite onde se encontra seu imóvel?");
-                String endereco= Entrada.nextLine();
+        resolve= Entrada.nextLine();
 
-                System.out.println("O imóvel está a venda?");
-                boolean avenda = Entrada.nextBoolean();
+            switch (op) {
+                case 1:
+                
+                System.out.println("Digite onde se encontra seu imóvel:");
+                endereco= Entrada.nextLine();
+                
+                    System.out.println("O imóvel está a venda?");
+                    resposta = Entrada.nextLine().toLowerCase();
+
+                    if (resposta.equals("sim") || resposta== "true") {
+                        avenda=true;
+                    }else{
+                        avenda=false;
+                    }
 
                 if (avenda) {
-                    
-                    System.out.println("Digite quanto você está cobrando");
-                    boolean preco= Entrada.nextBoolean();
+                    System.out.println("Digite quanto você está cobrando:");
+                    preco = Entrada.nextLine();
 
+                }else{
+                    preco=null;
                 }
                 System.out.println("O imóvel é novo?");
-                boolean nova = Entrada.nextBoolean();
-
-
-                System.out.println("Informe qual é seu imóvel?");
-                String imovel = Entrada.nextLine();
-
-
+                resposta = Entrada.nextLine().trim().toLowerCase();
                 
+                if (resposta.equals("sim") || resposta== "true") {
+                    nova=true;
+                }else{
+                    nova=false;
+                }
+                
+                System.out.println("Informe se o seu imóvel é uma casa ou apartamento:");
+                imovel = Entrada.nextLine();
+
+                Produto novoProduto = new Produto(info,imovel, endereco, preco, avenda, nova);
+                produtos.add(novoProduto.index(info,endereco, imovel, avenda, nova, preco));
+                
+                info=produtos.size();
+                req++;
                 
                 break;
              case 2:
-                
-                break;
+                if (req==0) {
+                    System.out.println("Err! Parece que você está tentando excluir sem ao menos ter criado um objeto!");                  
+                }else{
+                    System.out.println("Listando todos objetos...");
+                    System.out.println(produtos);
+                    System.out.println("Qual produto você deseja remover? (utilize o index para remover)");
+                    remove= Entrada.nextInt();
+                    if (remove>produtos.size() || remove<0) {
+                        System.out.println("Você digitou um número que não está no index");
+                    }else{
+                        produtos.remove(remove);
+                        System.out.println("Lista removida, formulários restantes:");
+                        System.out.println(produtos);
+                    }
+                }
+             break;
              case 3:
-                
-            break;
-        
-            case 4:
-
-            break;
-            default:
+                if (req==0) {
+                    System.out.println("Err! Parece que você está tentando listar sem ao menos ter criado um objeto!");                  
                 break;
-        }
+
+                }else{
+                    System.out.println(produtos);
+                    System.out.println("Pressione Enter para continuar...");
+                    Entrada.nextLine();
+                }
+                break;
+               
+             case 4:
+             
+             break;
+             default:
+             System.out.println("Digite uma das opções acima!");
+             break;
+             
+            }
+        }while(op!=4);
         }  
     }
--- ATVDS01/Produto.java
+++ ATVDS01/Produto.java
@@ -1,26 +1,25 @@
 public class Produto {
     private String endereco;
-    private int preco;
+    private String preco;
     private boolean avenda;
     private boolean nova;
     private String imovel;
+    private int info;
     
-    public Produto(String imovel,String endereco, int preco, boolean avenda, boolean nova){
+    public Produto(int info,String imovel,String endereco, String preco, boolean avenda, boolean nova){
         this.imovel = imovel;
         this.endereco = endereco;
         this.preco = preco;
         this.avenda = avenda;
         this.nova = nova;
     }
-    public Produto(String imovel,String endereco, boolean nova){
-        this.imovel = imovel;
-        this.endereco = endereco;
-        this.nova = nova;
+    public static String index(int info,String endereco, String imovel, boolean avenda, boolean nova, String preco) {
+        return "\n"+ "Index: "+ info+"\nEndereco: " + endereco + "\nTipo de imóvel: " + imovel + "\nÀ venda: " + avenda + "\nNovo: " + nova + "\nPreço: " + preco +"\n";
     }
     public String getEndereco(){
         return endereco;
     }
-    public int getPreco(){
+    public String getPreco(){
         return preco;
     }
     public boolean isAvenda(){
@@ -30,9 +29,21 @@ public class Produto {
         return nova;
     }
     public String getImovel(){
-        return imovel
+        return imovel;
     }
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String endereco) {
+        this.endereco = endereco;
+    }
+    public void setPreco(String preco){
+        this.preco= preco;
+    }
+    public void setAvenda(boolean avenda){
+        this.avenda= avenda;
+    }
+    public void setNova(boolean nova){
+        this.nova = nova;
+    }
+    public void setImovel(String imovel){
+        this.imovel=imovel;
     }
 }
